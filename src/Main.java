dbdcb3637f260f5650d63eea11d760cfeed8dad6
Julia Petersson <julia.gloggnitzer@outlook.com>
2022-09-08 13:16:55 +0200

    mer text

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,29 @@ public class Main {
         System.out.println("Jag behöver nog börja om");
 
         // ändring
+
+        // pwd - nuvarande mapp
+        //ls - lista allt i mappen
+        //ls - l = lista under varandra med mer info
+        //cd - change directory  - byta mapp
+        // cd Desktop till exempel - men det måste vara stavad som det är stavad i listan /autofyll genom tab
+        //clear - rensa terminalen
+        //cd .. = en upp i mappen
+        // git init - initiera
+        // alla filer som börjar på . göms automatiskt
+        //ls - a = visar även gömda filer
+        // Initiera våran project:
+        //- i början auf projectet och görs bara en gång- du måste kolla att du är i rätt katalog
+        // i detta fall test fjor Gihub
+        // sen skriver jag git init (skapa ett git project)
+        // git status - för att se vilken status det är
+        // info om att det finns commits elelr ej - och i röd detta som inte är comitted
+        // git add . = gör alla fila redo för att comittas
+        //git commit - m " " = man läggga in meddelandet om vad man har förändrad sen innan
+        // git log - dagboken quasi
+
+
+
         
     }
 }
